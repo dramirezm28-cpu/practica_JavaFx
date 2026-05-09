b6b92249cee47935494d3b3678d0611c6711b2a7
umg <umg@HUE-LAB-2F3.labhue.local>
2026-05-09 10:22:04 -0600

    Add JavaFX base window

--- practica_JavaFxx/src/main/java/com/practica/productos/app/Main.java
+++ practica_JavaFxx/src/main/java/com/practica/productos/app/Main.java
@@ -5,7 +5,7 @@ import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
-
+//ffhgh
 public class Main extends Application{
 @Override
 public void start(Stage stage) {
